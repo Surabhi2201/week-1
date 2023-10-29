ae0dabfa53b678a9ea43d8606d2e75c5f2176575
Surabhi2201 <143389194+Surabhi2201@users.noreply.github.com>
2023-10-29 16:52:31 +0530

    Update arithmetic.cpp

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -5,10 +5,10 @@ int main()
 	printf("Enter 2 numbers");
 	scanf("%d%d",&a,&b);
 	c=a+b,d=a-b, e=a*b, f=a/b;
-	printf("The sum of a and b is %d",c);
-	printf("The difference of a and b is %d /n",d );
-	printf("The multiplication of a and b is %d /n",e);
-	printf("Te division of a and b is %d",f);
+	printf("The sum of a and b is %d\n",c);
+	printf("The difference of a and b is %d \n",d );
+	printf("The multiplication of a and b is %d \n",e);
+	printf("The division of a and b is %d\n",f);
 	
 	return(0);
 	
