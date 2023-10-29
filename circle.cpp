0ffa47a1d3f2b5ca5f4a683829aee6edd4fc2fb9
Surabhi2201 <143389194+Surabhi2201@users.noreply.github.com>
2023-10-29 15:41:35 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int r;
+ printf("Enter The Radius:");
+ scanf("%d",&r);
+ float area;
+ area=3.14*r*r;
+ printf("The area of circle :%f",area);
+ return(0);
+}
---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ printf("Enter side of square:");
+ scanf("%d",&n);
+ int area=n*n;
+ printf("The area of square is:%d",area);
+ return(0);
+ 
+}
